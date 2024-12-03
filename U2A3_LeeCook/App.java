5fc74c0d29052c7f31a0ee07f491f993f89f01fc
lee <xlegendman@gmail.com>
2024-12-03 15:56:29 -0500

    U2A3

---
+++ U2A3_LeeCook/App.java
@@ -0,0 +1,15 @@
+package U2A3_LeeCook;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JFrame;
+
+public class App extends JFrame{
+    Path filePath = Paths.get("U2A3_LeeCook/inventory.txt"); // path to the inventory file.
+    App(){
+        //
+    }
+}
