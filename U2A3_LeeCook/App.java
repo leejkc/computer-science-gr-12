5bf7bfe68c111d217f7fd06db29c828d0a1bfbe1
lee <xlegendman@gmail.com>
2024-12-08 20:40:44 -0500

    U2A3

--- U2A3_LeeCook/App.java
+++ U2A3_LeeCook/App.java
@@ -1,8 +1,10 @@
 package U2A3_LeeCook;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class App extends JFrame{
@@ -10,6 +12,8 @@ public class App extends JFrame{
     final java.awt.Color textBackgroudColor = new java.awt.Color(213, 234, 245); // background color for text fields
 
     final JTextField name, cat, am, minimumAm, vendor, markup, discount; // cat means catagory, am means amount
+    final JTextArea output;
+    final JComboBox optionDropDown;
 
     App(){
         setSize(600,600); // set window size
@@ -19,11 +23,12 @@ public class App extends JFrame{
         getContentPane().setBackground(new java.awt.Color(125, 180, 209)); // background color of the content pane
         setLayout(null); // absolute positioning layout
 
-        JLabel header = new JLabel("Inventory Manager");
+        final JLabel header = new JLabel("Inventory Manager");
         header.setBounds(30, 10, 300, 40);
         header.setFont(new java.awt.Font("Consolas", java.awt.Font.BOLD, 30));
         add(header);
 
+        // creates label and textfields
         name = createLabelAndTextField("Name: ", 30, 60);
         cat = createLabelAndTextField("Catagory: ", 30, 90);
         am = createLabelAndTextField("Amount: ", 30, 120);
@@ -32,13 +37,44 @@ public class App extends JFrame{
         markup = createLabelAndTextField("Markup: ", 30, 210);
         discount = createLabelAndTextField("Discount: ", 30, 240);
         
-        JButton submit = new JButton("Submit Fields");
+        // JTextArea for output results.
+        output = new JTextArea();
+        output.setEditable(false);
+        output.setBounds(300, 60, 250, 300);
+        output.setFont(font);
+        output.setBackground(textBackgroudColor);
+        add(output);
+
+        // JComboBox for options once submitted text.
+        optionDropDown = new JComboBox<>();
+        optionDropDown.setBounds(30, 300, 250, 20);
+        optionDropDown.addItemListener(null);
+        add(optionDropDown);
+        optionDropDown.setVisible(false);
+
+        // button to select option from dropdown
+        final JButton selectOption = new JButton();
+        selectOption.setBounds(30, 330, 250, 20);
+        selectOption.setFont(font);
+        selectOption.setBackground(textBackgroudColor);
+        add(selectOption);
+        selectOption.setVisible(false);
+        selectOption.addActionListener(_->{
+            int i = optionDropDown.getSelectedIndex();
+        });
+
+        // button to submit textfields
+        final JButton submit = new JButton("Submit Fields");
         submit.setBounds(30, 270, 250, 20);
         submit.setFont(font);
         submit.setBackground(textBackgroudColor);
         add(submit);
         submit.addActionListener(_->{
-            ItemData.insertData(name.getText(), cat.getText(), am.getText(), minimumAm.getText(), vendor.getText(), markup.getText(), discount.getText());
+            optionDropDown.setVisible(ItemData.checkData(name.getText(), cat.getText().toUpperCase(), am.getText(), minimumAm.getText(), vendor.getText(), markup.getText(), discount.getText()));
+            selectOption.setVisible(ItemData.checkData(name.getText(), cat.getText().toUpperCase(), am.getText(), minimumAm.getText(), vendor.getText(), markup.getText(), discount.getText()));
+            // makes dropdown and button visible if all textfields are not empty
+            revalidate();
+            repaint();
         });
 
         setVisible(true);
@@ -53,13 +89,13 @@ public class App extends JFrame{
      * @see JTextField
      */
     private JTextField createLabelAndTextField(String labelText, int x, int y) {
-        JLabel label = new JLabel(labelText); // create label
+        final JLabel label = new JLabel(labelText); // create label
         label.setBounds(x, y, 130, 20); 
         label.setFont(font);
         add(label);
         
-        JTextField textField = new JTextField(); // create text field
-        textField.setBounds(x + 100, y - 2, 150, 20);
+        final JTextField textField = new JTextField(); // create text field
+        textField.setBounds(x + 100, y, 150, 20);
         textField.setFont(font);
         textField.setBackground(textBackgroudColor); 
         add(textField); 
--- U2A3_LeeCook/ItemData.java
+++ U2A3_LeeCook/ItemData.java
@@ -5,28 +5,101 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 public class ItemData {
-    public static void insertData(String name, String cat, String am, String minimumAm, String vendor, String markup, String discount){
-        Path filePath = Paths.get("U2A3_LeeCook/inventory.txt"); // path to the inventory file.
-        HashMap<String, String> dictionary = new HashMap<>();
 
+    /**
+     * 
+     * @param name
+     * @param cat
+     * @param am
+     * @param minimumAm
+     * @param vendor
+     * @param markup
+     * @param discount
+     * @return
+     */
+    public static boolean checkData(String name, String cat, String am, String minimumAm, String vendor, String markup, String discount){
         if (!(name.isBlank() || cat.isBlank() || am.isBlank() || minimumAm.isBlank() || vendor.isBlank() || markup.isBlank() || discount.isBlank())){
-            // if every variable prevuously listed is not blank
-            String data = name + ";" + am + ";" + minimumAm + ";" + vendor + ";" + markup + ";" + discount;
-            dictionary.put(cat, data);
-            try {
-                Files.writeString(filePath, dictionary, StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                e.printStackTrace(); // handle e -> print trace
-            }
-            //make so cat is in file and reads all lines for each cat.
+            return true;
         } else {
             JOptionPane.showMessageDialog(null, "All fields must be filled out.", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+
+    /**
+     * 
+     * @param name
+     * @param cat
+     * @param am
+     * @param minimumAm
+     * @param vendor
+     * @param markup
+     * @param discount
+     */
+    public static void write(String name, String cat, String am, String minimumAm, String vendor, String markup, String discount){
+        String id = cat.substring(0, 2);
+        String data = cat + "," + name + "," + am + "," + minimumAm + "," + vendor + "," + markup + "," + discount;
+    }
+
+    /**
+     * 
+     * @param removeMe
+     */
+    public static void remove(String removeMe) {
+        Path filePath = Paths.get("U2A3_LeeCook/inventory.txt"); // path to the inventory file.
+        try {
+            List<String> lines = Files.readAllLines(filePath); // read all lines from the file
+            List<String> newLines = new ArrayList<>(); // create a new list to store remaining lines
+            for (String line : lines) {
+                if (!line.startsWith(removeMe)) { // if the line does not start with, add it to the new list
+                    newLines.add(line);
+                }
+            }
+            Files.write(filePath, newLines); // overwrite the file with the new list of lines
+            return;
+        } catch (IOException e) {
+            e.printStackTrace(); // handle exception if theres an issue with file operations
             return;
         }
     }
+
+    /**
+     * "search" method to search items by id, catagory, or name.
+     * always searches in this order:
+     * <ul>
+     * <li><b>id</b>
+     * <li><b>name</b>
+     * <li><b>catagory</b>
+     * </ul>
+     * 
+     * <ul>
+     * <li>if user only knows id:
+     * <ul><li>look for line that starts with id
+     * <li>returns empty string
+     * </ul>
+     * <li>if user only knows name:
+     * <ul><li>splits by comma and compares index 1
+     * <li>returns empty string
+     * </ul>
+     * <li>if user only knows catagory:
+     * <ul><li>splits all lines by comma and looks for each line with the same catagory
+     * <li>returns a string with all those lines for the user to pick from.
+     * </ul>
+     * </ul>
+     * @param input represents id, catagory, or name
+     * @return String
+     */
+    public static String search(String input){
+        // display a dropdown?
+        // new window?
+        // make sure to update javadoc
+        return new String();
+    }
+    
 }
