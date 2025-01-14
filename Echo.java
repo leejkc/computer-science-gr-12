86b7e5141116f3891703c62d8838857725ee7cd1
lee <xlegendman@gmail.com>
2025-01-14 12:04:54 -0500

    remove others in stockly branch

--- Echo.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Echo {
-    public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog(null, "Write something...", "Echo", JOptionPane.ERROR_MESSAGE);
-        JOptionPane.showMessageDialog(null, input, "Echo", JOptionPane.WARNING_MESSAGE);
-    }
-}
